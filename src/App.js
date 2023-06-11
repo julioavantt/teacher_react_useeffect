3f01ac7228613e05902b2a3cc2a4cb309a736b0e
unknown <you@example.com>
2023-06-11 01:19:27 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,38 +1,59 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"
 
-import "./App.css";
+import "./App.css"
 
 function App() {
-  const [uno, setUno] = useState(false);
-  const [dos, setDos] = useState(false);
+	const [uno, setUno] = useState(false)
+	const [dos, setDos] = useState(false)
 
-  useEffect(() => {
-    // Se ejecuta despues de montarse el componente y en cada cambio de estado
-    console.log("Sin dependencias");
-  });
+	useEffect(() => {
+		//! Se ejecuta despues de montarse el componente y en cada cambio de estado
+		console.log("%cSIN DEPENDENCIAS", "background-color: yellow")
+	})
 
-  useEffect(() => {
-    // Se ejecuta sólo despues de montarse el componente
-    console.log("Array de dependencias vacías");
-  }, []);
+	useEffect(() => {
+		//! Se ejecuta sólo despues de montarse el componente
+		console.log("%c[]", "background-color: orange")
+	}, [])
 
-  useEffect(() => {
-    // Se ejecuta despues de montarse el componente y en cada cambio de estado de dependencias
-    console.log("Con dependencias");
-  }, [uno]);
+	useEffect(() => {
+		//! Se ejecuta despues de montarse el componente y en cada cambio de estado de dependencias
+		console.log(
+			"%cSE EJECUTA SOLO CUANDO HAY CAMBIOS EN uno",
+			"background-color: lightblue"
+		)
+	}, [uno])
 
- /* useEffect(() => {
-    // Función de limpieza
-    document.addEventListener('click', alert());
-    return () => document.removeEventListener('click', alert());
-  });*/
+	useEffect(() => {
+		const alertar = () => alert("HELP")
 
-  return (
-    <>
-      <button onClick={() => setUno(prev => !prev)}>Cambiar estado "uno"</button>
-      <button onClick={() => setDos(prev => !prev)}>Cambiar estado "dos"</button>
-    </>
-  );
+		const tres = document.querySelector(".box-3")
+
+		tres.addEventListener("click", alertar)
+
+		//! Función de limpieza
+		return () => tres.removeEventListener("click", alertar)
+	})
+
+	return (
+		<>
+			<div className="box-1">
+				<div className="btn btn-one" onClick={() => setUno(prev => !prev)}>
+					<span>UNO</span>
+				</div>
+			</div>
+			<div className="box-2">
+				<div className="btn btn-one" onClick={() => setDos(prev => !prev)}>
+					<span>DOS</span>
+				</div>
+			</div>
+			<div className="box-3">
+				<div className="btn btn-one">
+					<span>TRES</span>
+				</div>
+			</div>
+		</>
+	)
 }
 
-export default App;
+export default App
